aeec79e90f1cee713d4e84cc0c3063bcfccefe73
Edsey omenda <omendajames0317@gmail.com>
2022-10-07 17:53:24 +0300

    commit tree 6edbb1ad96dcac96e1359b65c651c1341495eb1b

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+    before_action :authorize
+
+    def index
+        render json: Recipe.all
+      end
+    
+      def create
+        recipe = @created_user.recipes.create!(recipe_params)
+        render json: recipe, status: :created
+      end
+    
+      private
+    
+      def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+      end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # resources :users, only: [:create, :show]
+
+  # resources :sessions, only: [:create, :destroy]
+
+  resources :recipes, only: [:index, :create]
+
+
+  get"/me", to: "users#show"
+  post "/signup", to: "users#create"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  # get "/recipes", to: "recipes#index"
+  # post "/reipes", to: "recipes#create"
 end
+
